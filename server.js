88ec80d17f5b29ea308d214f41f0e5475b0e3111
Asif Al Mahmud <asifalmahmud1990@gmail.com>
2022-08-08 11:03:27 +0600

    applied MVC pattern for /employees route

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const app = express();
 const path = require("path");
 const cors = require("cors");
+const corsOptions = require("./config/corsOptions");
 const { logger } = require("./middleware/logEvents");
 const errorHandler = require("./middleware/errorHandler");
 
@@ -16,27 +17,6 @@ const PORT = process.env.PORT || 3500;
 app.use(logger);
 
 //-> Example of third party middleware:
-const whitelist = [
-   "https://www.yoursite.com",
-   "http://127.0.0.1:5500",
-   "http://localhost:3500",
-]; // the first one is for the actual domain when we will be in the production mode, others are local
-const corsOptions = {
-   origin: (origin, callback) => {
-      // here origin parameter is for whoever have requested
-      console.log({ origin });
-      if (whitelist.indexOf(origin) !== -1 || !origin) {
-         // this condition makes sure - if the domain is in the whitelist then it will be let pass by calling the callback and passing true as the second parameter of the callback
-         // we need to add the "!origin" condition for development time, but before production we will have to remove it
-
-         callback(null, true);
-      } else {
-         callback(new Error("Not allowed by CORS"));
-      }
-   },
-   optionsSuccessStatus: 200,
-};
-
 app.use(cors(corsOptions)); // CORS stands for Cross Origin Resource Sharing
 
 //-> Examples of built-in middlewares:
